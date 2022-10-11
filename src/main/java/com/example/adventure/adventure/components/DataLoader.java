4f0b3650984723490afa8e23245501a9bb611b0f
the-scooby-gang <108406868+the-scooby-gang@users.noreply.github.com>
2022-10-11 11:18:02 +0100

    Seeded data

--- src/main/java/com/example/adventure/adventure/components/DataLoader.java
+++ src/main/java/com/example/adventure/adventure/components/DataLoader.java
@@ -37,16 +37,40 @@ public class DataLoader implements ApplicationRunner {
         Player player1 = new Player(500, "Johnny", 100);
         playerRepository.save(player1);
 
-        NPC npc1 = new NPC("The Projector", 50, 10);
+        NPC npc1 = new NPC("The Protector", 50, 10);
         npcRepository.save(npc1);
+
+        NPC npc2 = new NPC("Elving", 80, 10);
+        npcRepository.save(npc2);
+
+        NPC npc3 = new NPC("Yer Maw", 100, 60);
+        npcRepository.save(npc3);
 
         Weapon weapon1 = new Weapon(15, "sword");
         weaponRepository.save(weapon1);
 
-        Room room1 = new Room("Attack room", npc1);
+        Weapon weapon2 = new Weapon(25, "Axe");
+        weaponRepository.save(weapon2);
+
+        Weapon weapon3 = new Weapon(30, "Flail");
+        weaponRepository.save(weapon3);
+
+        Room room1 = new Room("Battle room", npc1);
         roomRepository.save(room1);
+
+        Room room2 = new Room("Puzzle room", npc3);
+        roomRepository.save(room2);
+
+        Room room3 = new Room("Boss room", npc2);
+        roomRepository.save(room3);
 
         Potion potion1 = new Potion("Love Potion", 50);
         potionRepository.save(potion1);
+
+        Potion potion2 = new Potion("Risky potion", -60);
+        potionRepository.save(potion2);
+
+        Potion potion3 = new Potion("Risky potion", 35);
+        potionRepository.save(potion3);
     }
 }
